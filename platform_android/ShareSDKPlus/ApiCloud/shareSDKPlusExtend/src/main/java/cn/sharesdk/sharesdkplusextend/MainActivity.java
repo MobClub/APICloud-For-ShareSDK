9765babc9693c6af62a963147f0b42bd62965e17
1019238091 <1019238091@qq.com>
2019-05-07 16:01:33 +0800

    新增android拓展平台

---
+++ platform_android/ShareSDKPlus/ApiCloud/shareSDKPlusExtend/src/main/java/cn/sharesdk/sharesdkplusextend/MainActivity.java
@@ -0,0 +1,11 @@
+package cn.sharesdk.sharesdkplusextend;
+
+import com.uzmap.pkg.uzcore.UZWebView;
+import com.uzmap.pkg.uzcore.uzmodule.UZModule;
+
+public class MainActivity extends UZModule {
+
+    public MainActivity(UZWebView webView) {
+        super(webView);
+    }
+}
